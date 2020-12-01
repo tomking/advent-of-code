438b20c4398fc9d9d81a5de4105df11e0cf95621
Thomas King <tomking127@gmail.com>
2020-12-01 00:37:58 -0500

    Implement solutions for first day

---
+++ Day1/sol.py
@@ -0,0 +1,21 @@
+def solve_part_one(input_list):
+    for first_number in input_list:
+        for second_number in input_list:
+            if first_number + second_number == 2020:
+                return first_number * second_number
+
+def solve_part_two(input_list):
+    for first_number in input_list:
+        for second_number in input_list:
+            for third_number in input_list:
+                if (first_number + second_number + third_number) == 2020:
+                    return first_number * second_number * third_number
+
+with open("input.txt") as input:
+    input_list = [int(line) for line in input]
+    input.close()
+
+print(f"Part One Solution: {solve_part_one(input_list)}")
+
+
+print(f"Part Two Solution: {solve_part_two(input_list)}")
