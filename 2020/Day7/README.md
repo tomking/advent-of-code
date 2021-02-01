7d532e101895530880eabc5abc5bf50d8ff57de7
Thomas King <tomking127@gmail.com>
2021-02-01 14:43:22 -0500

    Implement various boilerplates

---
+++ 2020/Day7/README.md
@@ -0,0 +1,3 @@
+# Day Seven
+
+Placeholder
---
+++ 2020/Day7/sol.py
@@ -0,0 +1,9 @@
+def main():
+    with open("input.txt") as input:
+        dirty_input = input.read()
+        input.close()
+
+    
+
+if __name__ == '__main__':
+    main()
